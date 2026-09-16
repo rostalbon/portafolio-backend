c600bce6a77607650e8d6a55e4f45d4dd7c388d6
Rocío <rostalbon@gmail.com>
2026-09-15 21:46:30 -0300

    Se modificaron los endpoints para poder trabajar con las id desde ahí

--- projects/urls.py
+++ projects/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import ProjectView
+from .views import ProjectView, ProjectDetailView
 
 urlpatterns = [
-    path('projects', ProjectView.as_view())
+    path('projects/', ProjectView.as_view()),
+    path('projects/<int:pk>/', ProjectDetailView.as_view())
 ]
